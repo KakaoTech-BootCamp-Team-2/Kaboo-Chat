436e0fb6534fe27c902c065341f31f1da3c104fa
박지혁 <pjh5365@naver.com>
2024-08-17 10:18:11 +0900

    Init: 초기 프로젝트 생성
    초기 의존성
    - Spring Web
    - Spring Data JPA
    - Spring Data Redis
    - Spring Data MongoDB
    - H2 Database
    - MariaDB Driver
    - Lombok

---
+++ src/main/java/kaboo/kaboochat/KabooChatApplication.java
@@ -0,0 +1,13 @@
+package kaboo.kaboochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabooChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabooChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaboo/kaboochat/KabooChatApplicationTests.java
@@ -0,0 +1,13 @@
+package kaboo.kaboochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabooChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
